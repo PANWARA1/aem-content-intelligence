5d5319bd52a251739b01a5b2010d798524d75ab2
sumantapakira <sumantapakira@gmail.com>
2017-02-14 12:41:43 +0100

    New File

---
+++ core/src/main/java/com/aem/sumanta/fileprovider/FileProvider.java
@@ -0,0 +1,18 @@
+package com.aem.sumanta.fileprovider;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
+/**
+ * @author Sumanta Pakira
+ *
+ */
+public interface FileProvider {
+	
+	InputStream getInputStream(
+            String bundleSymbolicName,
+            String filename,String lang,
+            Map<String,String> comments) throws IOException;
+
+}
